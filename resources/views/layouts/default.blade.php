81aaf2a044d6f375f2eec90798444bb22237877b
Rati Patel <rjp76@njit.edu>
2018-07-16 14:00:03 -0400

    made pages (blades, default layout, index), added jumbotron

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+@include('includes.head')
+<body>
+@include('includes.menu')
+<main rules="main">
+@yield('content')
+</main>
+@include('includes.jsfooter')
+@include('includes.footer')
+</body>
+</html> 
